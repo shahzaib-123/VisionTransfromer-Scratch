4c36ec39a4e0008064a5426982be6cbfe0d03c53
Shahzaib Asghar <79242717+shahzaib-123@users.noreply.github.com>
2024-11-06 07:40:52 +0000

    added req

---
+++ requirements.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#torch CPU - Linux
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+
+#torch GPU - linux cudo1.2.1
+#pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+
+pip3 install numpy pandas matplotlib scikit-learn
